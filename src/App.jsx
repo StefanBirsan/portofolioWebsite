79ea1c8882cd1bf110dc7bdda9283a59679270fd
Stefan Birsan <stefbir10@gmail.com>
2024-11-02 13:15:36 +0200

    📝 Project setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+    return (
+        <main className="max-w-7xl mx-auto">
+            <h1 className="text-2xl text-white underline">React App</h1>
+        </main>
+    )
+}
+export default App
